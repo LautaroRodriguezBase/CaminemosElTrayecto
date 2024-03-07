bb68338fd63fb534f1d355d1ad06ceb88a3a576b
LautaroRB <lerb.dev@gmail.com>
2024-03-07 00:57:44 -0300

    Controlador:     Se agregaron varios comentarios.     Se reorganizaron todas las funciones.     Se creo una funcion privada la cual devuelve los cursos de X docente con Y estado.

--- CaminemosElTrayecto/src/main/java/com/iset/caminemoseltrayecto/CaminemosElTrayecto.java
+++ CaminemosElTrayecto/src/main/java/com/iset/caminemoseltrayecto/CaminemosElTrayecto.java
@@ -27,19 +27,14 @@ public class CaminemosElTrayecto {
     }
 
     //creo que todas las funciones deberian ser static para invocarlas desde otro archivo
-    
-    public static void addAlumnoAlCurso(Alumno a, Curso c) {
-        if(a != null && c != null){
-            c.addAlumno(a);// Tambien se tiene que actualizar el archivo
-        }// Lanzar una excepcion
-    }
-
-    //Hacer polimorfismo del tipo sobrecarga de funciones para que alcance a todas las opciones
-    public static void addCurso(Curso c){//No se si es a los docentes, al alumno, a la propia universidad
-        //lo dejo como a la propia universidad, calculo que será llamada por otra funcion que complemente
-        cursos.add(c);
-    }
-
+    /*
+     Hay que ir acomodando las funciones en base a quien las puede llamar.
+     Sumado a esto el controlador tiene que tener todas las funciones de los modelos
+     ya que la visual invoca al controlador y este al modelo.
+     No todas son exclusivas de la clase ya que pueden ser invocadas por el admin.
+     Es para tener una referencia
+     */
+    // Funciones del Controlador
     public static User existe(String uName, String uPass) {
         /*
         Antes de eso tiene que llenar el array de usuarios leyendolo del archivo.
@@ -57,7 +52,51 @@ public class CaminemosElTrayecto {
         //Crear excepciones para estos casos
         throw new UnsupportedOperationException("No existe el usuario");//Crear un archivo de idiomas para los Strings y demas
     }
+    public static boolean ingresar(String tfU, String tfP){
+        //Faltaria definir bien como funciona con la funcion "existe("
+        if(tfU.equals("Admin") && tfP.equals("1234")){
+            new AdminFrame(new Admin(tfU, tfP)).setVisible(true);
+            return true;
+        }else{// teoricamente aca deberia lanzar una excepcion
+            return false;
+        }
+    }
+            //Esta pertenece al Controlador
+    private static ArrayList<Curso> getCursosConEstadoEn(ArrayList<Curso> cursosConEstadoEn, String estado){
+        for(Curso c : CaminemosElTrayecto.cursos){
+            if(c.getEstadoCurso().equals(estado)){
+                cursosConEstadoEn.add(c);
+            }
+        }
+        return cursosConEstadoEn;
+    }
 
+    // Funcionesd del Admin
+    public static void habilitarCurso(Curso c){//la ejecuta el administrador.
+                                               //Deberia tambien el Docente pero la visual deberia mostrarle solo los cerrados
+        c.cambiarEstado("Habilitado");//el estado del curso deberia ser un int, y de ahi asociarlo a un Stirng
+    }
+        //Medio al pedo la de arriba si basicamente lo hace la de abajo. El nombre de la de arriba tiene que ir en el btn de la visual
+    public static void cambiarEstadoDelCurso(Curso c, String estado) {
+        c.cambiarEstado(estado);
+    }
+    public static void blanquearPass(User u, String DNI){//hay dos opciones, o se pone el DNI como atributo de USER, o se pasa el DNI como parametro
+        u.setUserPass(DNI);
+    }
+    public static void sancionarA(Sancionable s){
+        s.sancionar();//EL docente y alumno deberian tener una variable para saber su estado o un bool para saber si estan sancionados
+    }
+    public static void quitarSancionA(Sancionable s){
+        s.quitarSancion();
+    }
+    
+    // Funciones del Alumno
+    public static void addAlumnoAlCurso(Alumno a, Curso c) {
+        if(a != null && c != null){
+            c.addAlumno(a);// Tambien se tiene que actualizar el archivo
+        }// Lanzar una excepcion
+    }
+    
     public static ArrayList<Curso> verCursosDisponibles(Alumno a){
         ArrayList<Curso> cursosDisponibles = new ArrayList<Curso>();
         
@@ -88,22 +127,27 @@ public class CaminemosElTrayecto {
         return a.getCursosAprobados();
     }
 
-    public static Docente verCursosHabilitadosYCerrados(Docente d){//Porque habilitados y cerrados?
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public static void proponerCurso(Docente d, Curso c){//la ejecuta el docente
-        d.addCurso(c); //no deberia retornar nada
-    }
-
-    public static void habilitarCurso(Curso c){//la ejecuta el administrador
-        c.cambiarEstado("Habilitado");//el estado del curso deberia ser un int, y de ahi asociarlo a un Stirng
-    }
-
     public static ArrayList<Curso> verCursosInscriptos(Alumno a){
         return a.getCursosInscriptos();
     }
 
+        // Esta tambien la invoca el Docente y Admin
+    public static void changePass(User u, String pass){
+        if(!pass.equals("") && pass.length() >= 4){
+            u.setUserPass(pass);
+        }else{
+            //lanza una excepcion
+        }
+    }
+    
+// Funciones del Docente
+    public static void addCurso(Curso c){//No se si es a los docentes, al alumno, a la propia universidad
+        //lo dejo como a la propia universidad, calculo que será llamada por otra funcion que complemente
+        cursos.add(c);
+    }
+    public static void proponerCurso(Docente d, Curso c){//la ejecuta el docente
+        d.addCurso(c); //no deberia retornar nada
+    }
     public static void finalizarCurso(Curso c, ArrayList<Alumno> alumnosAprobados){//El array list que recibe es de los alumnos aprobados
         c.cambiarEstado("Finalizado");
         for(Alumno a : alumnosAprobados){
@@ -114,37 +158,11 @@ public class CaminemosElTrayecto {
         }
     }
 
-    public static void blanquearPass(User u, String DNI){//hay dos opciones, o se pone el DNI como atributo de USER, o se pasa el DNI como parametro
-        u.setUserPass(DNI);
+        //Estas dos funciones no aparecen en el UML
+    public static ArrayList<Curso> verCursosHabilitados(Docente d){
+        return getCursosConEstadoEn(d.getCursosCreados(), "Habilitado");//Crear final para los txt
     }
-
-    public static void cambiarEstadoDelCurso(Curso c, String estado) {
-        c.cambiarEstado(estado);
-    }
-
-    public static void sancionarA(Sancionable s){
-        s.sancionar();//EL docente y alumno deberian tener una variable para saber su estado o un bool para saber si estan sancionados
-    }
-
-    public static void quitarSancionA(Sancionable s){
-        s.quitarSancion();
-    }
-
-    public static boolean ingresar(String tfU, String tfP){
-        
-        if(tfU.equals("Admin") && tfP.equals("1234")){
-            new AdminFrame(new Admin(tfU, tfP)).setVisible(true);
-            return true;
-        }else{// teoricamente aca deberia lanzar una excepcion
-            return false;
-        }
-    }
-
-    public static void changePass(User u, String pass){
-        if(!pass.equals("") && pass.length() >= 4){
-            u.setUserPass(pass);
-        }else{
-            //lanza una excepcion
-        }
-    }
+    public static ArrayList<Curso> verCursosCerrados(Docente d){
+        return getCursosConEstadoEn(d.getCursosCreados(), "Cerrado");
+    } 
 }
